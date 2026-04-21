f0720c981fa021f2b5520ec9da190264edad61c8
Muwatta <abdullahmusliudeen@gmail.com>
2026-04-21 23:00:33 +0100

    fix: vercel config and database setup

--- hng_stage2/wsgi.py
+++ hng_stage2/wsgi.py
@@ -1,17 +1,15 @@
-"""
-WSGI config for hng_stage2 project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hng_stage2.settings')
+
+# Run migrations on first request (only if on Vercel)
+if os.environ.get('VERCEL') or os.environ.get('VERCEL_ENV'):
+    from django.core.management import call_command
+    try:
+        call_command('migrate', '--noinput')
+    except Exception:
+        pass
 
 application = get_wsgi_application()
 app = application
