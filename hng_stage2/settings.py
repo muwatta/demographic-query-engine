6fc905b72930f1e5a32a0de1268e484ad6ee1481
Muwatta <abdullahmusliudeen@gmail.com>
2026-04-23 15:25:19 +0100

    updated the Debug

--- hng_stage2/settings.py
+++ hng_stage2/settings.py
@@ -5,7 +5,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-trn_)g)xi$dcqx)9m&@0ahjkr-*i8cdr(s8v#@!l8t^7@@1-3g'
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
